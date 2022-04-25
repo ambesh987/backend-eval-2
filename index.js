04886b7e27011447ec3fd941b73f12ee88971edf
ambesh987 <54496820+ambesh987@users.noreply.github.com>
2022-04-25 21:35:14 +0530

    added

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const dbconnect = require("./DBconnect/server");
 const app = express();
 const userRoutes = require("./Routes/userRoutes");
-
+const masterAccRoutes = require("./Routes/masterAcc");
 app.use(express.json());
 
+app.use("/user", userRoutes);
+app.use("/createmasteracc", masterAccRoutes);
 app.use("/user", userRoutes);
 
 const PORT = 3000;
