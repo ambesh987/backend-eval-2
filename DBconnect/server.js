750cb1fa3554ba0a62d75a61cd81887782f4fcd5
ambesh987 <54496820+ambesh987@users.noreply.github.com>
2022-04-25 21:51:37 +0530

    added commits

--- DBconnect/server.js
+++ DBconnect/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = () => {
   mongoose
     .connect(
-      "mongodb+srv://ambeshpm:Ambeshpmxd3458@cluster0.brhhe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+      "mongodb+srv://ambeshpm:Ambeshpm%402304@cluster0.qtsug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     )
     .then(console.log("DB Connected"));
 };
